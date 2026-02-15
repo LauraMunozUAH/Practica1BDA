6da296d6cd48c6a6cf78d8d1cbb604ab04f388aa
LauraMunozUAH <l.m.centenera@gmail.com>
2026-02-15 22:45:21 +0100

    9,10,11

--- Cuestiones.sql
+++ Cuestiones.sql
@@ -31,28 +31,33 @@ SELECT
 FROM cuestiones.estudiantes;
 
 --CUESTIÓN 3
--- Encontrar los estudiantes con índice 500
+----Consulta de estudiantes con índice 500
+SELECT *
+FROM cuestiones.estudiantes
+WHERE indice = 500;
+
+-- Número de tuplas encontradas
 SELECT COUNT(*) AS num_tuplas
 FROM cuestiones.estudiantes
 WHERE indice = 500;
 
+-- Tamaño total de la tabla en bloques
 SELECT pg_relation_size('cuestiones.estudiantes') / 8192 AS bloques; -- comprobar número de bloques total
-
-
+--Bloques realmente leídos durante la consulta
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT *
 FROM cuestiones.estudiantes
 WHERE indice = 500;
 
+--Estadísticas de la tabla
 SELECT *
 FROM pg_stat_user_tables
 WHERE relname = 'estudiantes';
-
+--Estadísticas de columnas
 SELECT *
 FROM pg_stats
 WHERE tablename = 'estudiantes';
-
-
+--Estadísticas específicas del campo indice
 SELECT
     attname,
     n_distinct,
@@ -96,7 +101,6 @@ SELECT pg_relation_size('cuestiones.estudiantes2') / 8192 AS bloques; -- comprob
 SELECT COUNT(*) AS num_tuplas
 FROM cuestiones.estudiantes2
 WHERE indice = 500;
-
 
 EXPLAIN (ANALYZE, BUFFERS)
 SELECT *
@@ -144,3 +148,84 @@ VALUES ('Roberto Pérez', 10, 22, 450);
 SELECT ctid, *
 FROM cuestiones.estudiantes
 WHERE nombre = 'Roberto Pérez'; -- buscar la dirección física de la fila
+
+
+--CUESTIÓN 9
+VACUUM FULL cuestiones.estudiantes;
+ANALYZE cuestiones.estudiantes;
+--Comprobación antes y después
+SELECT pg_relation_size('cuestiones.estudiantes') / 8192 AS bloques;
+-- Comprobamos que no hay dead_tup
+SELECT n_live_tup, n_dead_tup
+FROM pg_stat_user_tables
+WHERE relname = 'estudiantes';
+
+
+--CUESTIÓN 10
+CREATE TABLE cuestiones.estudiantes3 (
+    estudiante_id SERIAL,
+    nombre VARCHAR(40),
+    codigo_carrera INT,
+    edad INT,
+    indice INT
+) PARTITION BY HASH (codigo_carrera);
+DO $$
+BEGIN
+  FOR i IN 0..19 LOOP
+    EXECUTE format(
+      'CREATE TABLE cuestiones.estudiantes3_p%s
+       PARTITION OF cuestiones.estudiantes3
+       FOR VALUES WITH (MODULUS 20, REMAINDER %s);',
+      i, i
+    );
+  END LOOP;
+END $$;
+
+COPY cuestiones.estudiantes3(nombre, codigo_carrera, edad, indice)
+FROM 'C:\estudiantes.csv'
+WITH (FORMAT csv);
+
+--Ver cuantos bloques ocupa cada partición
+SELECT
+    relname,
+    pg_relation_size(oid) / 8192 AS bloques
+FROM pg_class
+WHERE relname LIKE 'estudiantes3_p%';
+
+
+--CUESTIÓN 11
+----Consulta de estudiantes con índice 500
+SELECT *
+FROM cuestiones.estudiantes3
+WHERE indice = 500;
+
+-- Número de tuplas encontradas
+SELECT COUNT(*) AS num_tuplas
+FROM cuestiones.estudiantes3
+WHERE indice = 500;
+
+-- Tamaño total de la tabla en bloques
+SELECT pg_relation_size('cuestiones.estudiantes3') / 8192 AS bloques; -- comprobar número de bloques total
+--Bloques realmente leídos durante la consulta
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT *
+FROM cuestiones.estudiantes3
+WHERE indice = 500;
+
+--Estadísticas de la tabla
+SELECT *
+FROM pg_stat_user_tables
+WHERE relname = 'estudiantes3';
+--Estadísticas de columnas
+SELECT *
+FROM pg_stats
+WHERE tablename = 'estudiantes3';
+--Estadísticas específicas del campo indice
+SELECT
+    attname,
+    n_distinct,
+    most_common_vals,
+    most_common_freqs
+FROM pg_stats
+WHERE tablename = 'estudiantes3'
+  AND attname = 'indice';
