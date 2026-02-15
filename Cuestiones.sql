1fba7bf4dca5bdd06bb1f6b071380742a1217985
inesperezpascual <ines.perezpascual@edu.uah.es>
2026-02-15 14:24:09 +0100

    Ejercicios 3,4 y casi 5

--- Cuestiones.sql
+++ Cuestiones.sql
@@ -32,6 +32,57 @@ FROM cuestiones.estudiantes;
 
 --CUESTIÓN 3
 -- Encontrar los estudiantes con índice 500
+SELECT COUNT(*) AS num_tuplas
+FROM cuestiones.estudiantes
+WHERE indice = 500;
+
+SELECT pg_relation_size('cuestiones.estudiantes') / 8192 AS bloques; -- comprobar número de bloques total
+
+
+EXPLAIN (ANALYZE, BUFFERS)
 SELECT *
 FROM cuestiones.estudiantes
 WHERE indice = 500;
+
+SELECT *
+FROM pg_stat_user_tables
+WHERE relname = 'estudiantes';
+
+SELECT *
+FROM pg_stats
+WHERE tablename = 'estudiantes';
+
+
+SELECT
+    attname,
+    n_distinct,
+    most_common_vals,
+    most_common_freqs
+FROM pg_stats
+WHERE tablename = 'estudiantes'
+  AND attname = 'indice';
+
+--CUESTIÓN 4
+-- Repetir la consulta de la cuestión 3 y comparar
+SHOW shared_buffers; -- comprobar tamaño
+
+--CUESTIÓN 5
+CREATE TABLE IF NOT EXISTS cuestiones.estudiantes2 (
+    estudiante_id SERIAL,
+    nombre VARCHAR(40),
+    codigo_carrera INT,
+    edad INT,
+    indice INT
+);
+
+COPY cuestiones.estudiantes2(nombre, codigo_carrera, edad, indice)
+FROM 'C:\estudiantes.csv'
+WITH (FORMAT csv, HEADER false);
+
+DROP INDEX cuestiones.idx_estudiantes2_indice;
+CREATE INDEX IF NOT EXISTS idx_estudiantes2_indice
+ON cuestiones.estudiantes2(indice);
+
+CLUSTER cuestiones.estudiantes2 USING cuestiones.estudiantes2.idx_estudiantes2_indice;
+
+-- comprobar número de bloques total
